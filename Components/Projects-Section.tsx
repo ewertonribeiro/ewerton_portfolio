7a1ebf2bda1ba290ad3041f5c68604deddcbfce6
Ewerton RIbeiro <ewertondosanjosz@gmail.com>
2022-06-11 19:34:34 -0300

    Card Individual do projeto

--- Components/Projects-Section.tsx
+++ Components/Projects-Section.tsx
@@ -1,10 +1,14 @@
 import styles from "../styles/Projetcs-Section.module.css"
+import Project from "./Project"
 
 const ProjectsSection = () => {
   return (
     <section className={styles.projects_container}>
       <h2>Projetos</h2>
-
+      <main>
+        <Project />
+        <Project />
+      </main>
     </section>
   )
 }
