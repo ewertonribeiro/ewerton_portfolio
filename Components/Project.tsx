4fc9b5d7749ca59c75e5e02acce4672c145c9d29
Ewerton RIbeiro <ewertondosanjosz@gmail.com>
2022-06-13 13:00:06 -0300

    Redefinição da Margin esquerda

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -1,17 +1,16 @@
-import styles from "../styles/Project-Single.module.css"
+import styles from "../styles/Project-Single.module.css";
 
 const Project = () => {
   return (
     <div className={styles.project_card}>
       <div className={styles.img_wrapper}></div>
       <h3 className={styles.project_title}>Titulo</h3>
-      <p>Using React, Node.js,
-        Express & MongoDB you'll
-        learn how to build a Full Stack
-        MERN Application - from start to finish.
-        The App is called Memories and it is a simple social
-        media app that allows users to post interesting events
-        that happened in their lives.</p>
+      <p>
+        Using React, Node.js, Express & MongoDB you'll learn how to build a Full
+        Stack MERN Application - from start to finish. The App is called
+        Memories and it is a simple social media app that allows users to post
+        interesting events that happened in their lives.
+      </p>
       <ul>
         <li>Mongo</li>
         <li>React</li>
@@ -19,10 +18,12 @@ const Project = () => {
         <li>Express</li>
       </ul>
       <footer className={styles.project_footer}>
-        <a href="#" target="_blank">Code</a>
+        <a href="#" target="_blank">
+          Code
+        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Project;
